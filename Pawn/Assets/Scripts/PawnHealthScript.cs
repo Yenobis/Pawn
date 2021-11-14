0c111c56500583b45d8c4a550dc94332af809584
DT3MP01 <85438178+DT3MP01@users.noreply.github.com>
2021-11-14 13:42:13 +0100

    HUD

--- Pawn/Assets/Scripts/PawnHealthScript.cs
+++ Pawn/Assets/Scripts/PawnHealthScript.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PawnHealthScript : MonoBehaviour
 {
+    public GameObject[] vidas;
+    public int life;
     // Start is called before the first frame update
     void Start()
     {
-        
+        life = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (life <= 80)
+        {
+            vidas[4].gameObject.GetComponent<Image>().color = Color.white;           
+        }
+        if (life <= 60)
+        {
+            vidas[3].gameObject.GetComponent<Image>().color = Color.white;
+        }
+        if (life <= 40)
+        {
+            vidas[2].gameObject.GetComponent<Image>().color = Color.white;
+        }
+        if (life <= 20)
+        {
+            vidas[1].gameObject.GetComponent<Image>().color = Color.white;
+        }
+        if (life == 0)
+        {
+            vidas[0].gameObject.GetComponent<Image>().color = Color.white;
+        }
+    }
+    public void TakeDamage(int amount)
+    {
+        life -= amount;
     }
 }
+
