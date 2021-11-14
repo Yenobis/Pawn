546d5d1e57d7db7214a0ae41c9cee57074b49004
DT3MP01 <85438178+DT3MP01@users.noreply.github.com>
2021-11-14 14:03:58 +0100

    HUD

--- Pawn/Assets/Scripts/PawnHealthScript.cs
+++ Pawn/Assets/Scripts/PawnHealthScript.cs
@@ -7,6 +7,7 @@ public class PawnHealthScript : MonoBehaviour
 {
     public GameObject[] vidas;
     public int life;
+    public int stamina;
     // Start is called before the first frame update
     void Start()
     {
