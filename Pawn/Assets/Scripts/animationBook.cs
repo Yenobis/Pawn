e6e2db08f8d9730c9c096d1c9adc96c386fb949d
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2021-12-01 16:27:56 +0100

    libro con animaciones ya hechas
    se abre con la O y se cierra con la C

---
+++ Pawn/Assets/Scripts/animationBook.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationBook : MonoBehaviour
+{
+
+        Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isOpen = animator.GetBool("isOpen");
+
+        if (Input.GetKey("o")){
+            animator.SetBool("isOpen", true);
+        }
+        
+        if (Input.GetKey("c")){
+            animator.SetBool("isOpen", false);
+        }
+
+
+    }
+}
