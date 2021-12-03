91c11cffbfe563960b99700f102816ae330f737e
Jennifercs07 <jenni_canuto_7@hotmail.com>
2021-12-03 10:27:50 +0100

    SoundManager script

--- Pawn/Assets/Scripts/SoundManager.cs
+++ Pawn/Assets/Scripts/SoundManager.cs
@@ -1,26 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+public enum Sonidos { MOVERESPADA, TOTAL_SONIDOS };
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip moverEspadaSound;
-    public AudioClip choqueEspadaSound;
+    public AudioSource[] audios;
 
-    AudioSource audio;
+    string[] nombreSonidos = { "MoverEspada.mp3"};
 
     //Reconocer el componente AudioSource
     void Start()
     {
-        audio = GetComponent<AudioSource>();
+        audios = GetComponents<AudioSource>();
+
+        for (int i = (int) Sonidos.MOVERESPADA; i < (int) Sonidos.TOTAL_SONIDOS; i++)
+        {
+            audios[i] = AdComponent<AudioSource>();
+            audios[i].clip = Resources.Load<AudioClip>(nombreSonidos[i]);
+        }
+    }
+
+    private T AdComponent<T>()
+    {
+        throw new NotImplementedException();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))  //Detecta la pulsacion del clic izquierdo
         {
-            audio.clip = moverEspadaSound;
-            audio.Play();
+            if (audios[(int) Sonidos.MOVERESPADA].isPlaying) //Si el audio se esta reproduciendo
+            {
+                audios[(int) Sonidos.MOVERESPADA].Pause(); 
+            } else
+            {
+                audios[(int)Sonidos.MOVERESPADA].Play();
+            }
         }
     }
 }
