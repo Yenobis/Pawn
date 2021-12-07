51b2932fccd7247933ea2ea7b073fa8e1c6b38fd
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2021-12-07 22:28:55 +0100

    Update 3.7
    Flecha que apunta a los enemigos actualizada

--- Pawn/Assets/Scenes/AI Testing/DirectionalArrow.cs
+++ Pawn/Assets/Scenes/AI Testing/DirectionalArrow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +6,60 @@ using UnityEngine;
 public class DirectionalArrow : MonoBehaviour
 {
     [SerializeField]
-    private Transform target;
+    private Transform[] target;
     private Transform player;
+    private int puntero = 0;
+    private float minima = 0;
+    private Material[] m;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m = gameObject.GetComponent<MeshRenderer>().materials;
+        player = GameObject.Find("Pawn").transform;
+        minima = Vector3.Distance(player.position, target[0].transform.position);
+        puntero = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         player = GameObject.Find("Pawn").transform;
-        Vector3 targetPosition = target.transform.position;
-        //Debug.Log(targetPosition);
+        try
+        {
+            minima = Vector3.Distance(player.position, GameObject.Find(target[0].name).transform.position);
+            puntero = 0;
+        } catch (MissingReferenceException)
+        {
+            minima = float.MaxValue;
+            puntero = -1;
+        }
+        for (int i = 1; i < target.Length; i++)
+        {
+            try
+            {
+                Vector3 targetPosition = GameObject.Find(target[i].name).transform.position;
+                if (Vector3.Distance(player.position, targetPosition) < minima)
+                {
+                    minima = Vector3.Distance(player.position, targetPosition);
+                    puntero = i;
+                }
+            }
+            catch (MissingReferenceException)
+            {
+                // Está muerto
+            }
+        }
+
         //targetPosition.y = player.position.y;
-        transform.LookAt(2 * player.position - target.transform.position);
+        try
+        {
+            transform.LookAt(2 * player.position - target[puntero].transform.position);
+            gameObject.GetComponent<MeshRenderer>().material = m[0];
+        } catch (IndexOutOfRangeException)
+        {
+            //gameObject.transform.localScale = new Vector3(0f, 0f, 0f);
+            gameObject.GetComponent<MeshRenderer>().material = m[1];
+            //transform.LookAt(2 * player.position - new Vector3(0,10000,0));
+        }
     }
 }
