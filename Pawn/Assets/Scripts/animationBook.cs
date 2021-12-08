5856a09f5fe6f988c08a600a8fb3592158a871b1
DT3MP01 <85438178+DT3MP01@users.noreply.github.com>
2021-12-08 18:40:59 +0100

    Libro en NewMenu

--- Pawn/Assets/Scripts/animationBook.cs
+++ Pawn/Assets/Scripts/animationBook.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class animationBook : MonoBehaviour
 {
-
-        Animator animator;
+    [SerializeField] GameObject canvas;
+    Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
         animator = GetComponent<Animator>();
+        animator.SetBool("isOpen", true);
+        StartCoroutine(lanza());
 
+    }
+    public IEnumerator lanza()
+    {
+        yield return new WaitForSeconds(3.6f);
+        canvas.SetActive(true);
     }
 
     // Update is called once per frame
