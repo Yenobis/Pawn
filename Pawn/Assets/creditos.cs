b239012dc804934f4d994efb85dbbf22076552ab
Siboney <siboneyluismartin@gmail.com>
2021-12-15 20:33:10 +0000

    Pantalla final

---
+++ Pawn/Assets/creditos.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creditos : MonoBehaviour
+{
+    // Start is called before the first frame update
+public void Quit () 
+{
+    Debug.Log("Has salido");
+    Application.Quit();
+ }
+}
