bffcd386853099099d6eb107fc01c4acfe5c4c94
Jennifercs07 <jenni_canuto_7@hotmail.com>
2022-01-19 09:26:55 +0100

    Sonido del boss

---
+++ Pawn/Assets/Scripts/Sound_boss.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_boss : MonoBehaviour
+{
+    public enum Sonidos
+    {
+        GOLPE,
+        CAMINAR,
+        MUERTEENEMIGO,
+        TOTAL_SONIDOS
+    }
+
+    private bool isWalking, isRunning, isAttacking;
+    private float cur_health;
+    private bool alreadyDead = false;
+
+    public AudioSource[] audios;
+    string[] nombreSonidos = { "Golpe", "Caminar", "MuerteEnemigo" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        audios = GetComponents<AudioSource>();
+
+        for (int i = (int)Sonidos.GOLPE; i < (int)Sonidos.TOTAL_SONIDOS; i++)
+        {
+            audios[i].clip = Resources.Load<AudioClip>(nombreSonidos[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BossAI variables = gameObject.GetComponent<BossAI>();
+        isWalking = variables.isWalking;
+        isRunning = variables.isRunning;
+        cur_health = variables.cur_health;
+        isAttacking = GetComponentInChildren<Sword>().atacando;
+        //isAttacking = variables.isAttacking;
+        if (Time.timeScale == 1f)
+        {
+            if (!isAttacking && (isWalking || isRunning))
+            {
+                if (!audios[(int)Sonidos.CAMINAR].isPlaying)
+                {
+                    audios[(int)Sonidos.CAMINAR].Play();
+                }
+            }
+            if (isAttacking)
+            {
+                audios[(int)Sonidos.CAMINAR].Pause();
+                if (!audios[(int)Sonidos.GOLPE].isPlaying)
+                {
+                    audios[(int)Sonidos.GOLPE].time = 0.1f;
+                    audios[(int)Sonidos.GOLPE].Play();
+                }
+            }
+            if (cur_health <= 0 && !alreadyDead)
+            {
+                alreadyDead = true;
+                audios[(int)Sonidos.MUERTEENEMIGO].Play();
+            }
+        }
+        else
+        {
+            audios[(int)Sonidos.CAMINAR].Pause();
+            audios[(int)Sonidos.GOLPE].Pause();
+            audios[(int)Sonidos.MUERTEENEMIGO].Pause();
+        }
+    }
+}
